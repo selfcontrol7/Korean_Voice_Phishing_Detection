dbc4e9ea9e29b041f888946a97dee77f58b8f002
Milandu Keith Moussavou Boussougou <mbmk92@gmail.com>
2023-12-09 02:41:15 +0900

    # Data Augmentation

---
+++ Optimized_models/test.py
@@ -0,0 +1,14 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+ 
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+ 
+driver.get("https://python.org")
+print(driver.title)
+driver.close()
